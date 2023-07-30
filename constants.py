f922b92ea5d94b075197ebaa165632184e7bdf0f
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-07-30 17:08:16 -0400

    Add new paths to constants

--- constants.py
+++ constants.py
@@ -1,7 +1,20 @@
 import os
 
-TEMP_PATH = os.path.join(".", "temp")
+
+# App Path
+APP_PATH = os.path.join("D:", "TRACKS")
+
+
+# Folders
+TOPS_PATH = os.path.join(APP_PATH, "tops")
+DOWNLOADS_PATH = os.path.join(APP_PATH, "downloads")
+TEMP_PATH = os.path.join(APP_PATH, "temp")
+
+# Temp Files
 TEMP_IMAGE_NAME = "image.jpg"
 TEMP_IMAGE_PATH = os.path.join(TEMP_PATH, TEMP_IMAGE_NAME)
+
 TEMP_MP4_NAME = "temp.mp4"
 TEMP_MP4_PATH = os.path.join(TEMP_PATH, TEMP_MP4_NAME)
+
+
