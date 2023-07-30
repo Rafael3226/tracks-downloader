14923b20469e3c300f2afa3c1d7ba3cc364fadc7
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-07-30 15:47:41 -0400

    Add new constants

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
+import os
 
-TEMP_IMAGE_PATH = "./temp/image.jpg"
+TEMP_PATH = os.path.join(".", "temp")
+TEMP_IMAGE_NAME = "image.jpg"
+TEMP_IMAGE_PATH = os.path.join(TEMP_PATH, TEMP_IMAGE_NAME)
+TEMP_MP4_NAME = "temp.mp4"
+TEMP_MP4_PATH = os.path.join(TEMP_PATH, TEMP_MP4_NAME)
