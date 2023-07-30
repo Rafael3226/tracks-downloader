1ed0218ea3fef42f86f19dcad33ef9c0550dd366
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-07-30 17:09:07 -0400

    New main and workspace.xml

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-import os
-
-from albumCover import add_image_to_audio
-from fileManager import create_directory, walk_and_process_directory, get_file_name, process_json_file
-from youtube import search_youtube_video_url, download_youtube_video
+from constants import TOPS_PATH
+from fileManager import walk_and_process_directory
+from genre import Genre
 
 
-def download_top_100():
-    for file_path in genres_paths:
-        track_list = process_json_file(file_path)
-        genre = get_file_name(file_path)
-        path = os.path.join(DOWNLOADS_PATH, genre)
-        create_directory(path)
-
-        for track in track_list:
-            title = track["title"]+" - "+track["artist"]
-            audio_path = os.path.join(path, title)
-            video_url = search_youtube_video_url(title)
-            download_youtube_video(video_url, audio_path)
-            add_image_to_audio(audio_path, track["imageURL"])
+def download_all_top_100(files_paths):
+    for file_path in files_paths:
+        g = Genre(file_path)
+        g.download_track_list()
 
 
-# Example usage
 if __name__ == "__main__":
-    APP_PATH = os.path.join("D:", "TRACKS")
-    DOWNLOADS_PATH = os.path.join(APP_PATH, "downloads")
-    TOPS_PATH = os.path.join(APP_PATH, "tops")
-
-    genres_paths = walk_and_process_directory(TOPS_PATH)
-    download_top_100()
+    genres = walk_and_process_directory(TOPS_PATH)
+    download_all_top_100(genres)
