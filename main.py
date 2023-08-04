1eaee96dcd45f7d9432137554ca75eba27501e7e
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-08-04 01:19:51 -0400

    Move threads to main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from constants import TOPS_PATH
+from concurrent.futures import ThreadPoolExecutor
+
+from constants import TOPS_PATH, MAX_WORKERS
 from fileManager import get_files_from_dir
 from genre import Genre
 from models.database import db  # , Genre as GenreDB, Track as TrackDB
@@ -8,12 +10,14 @@ from threads import thread_pool
 def download_all_top_100(files_paths):
     for file_path in files_paths:
         g = Genre(file_path)
-        g.download_track_list()
+        thread_pool.submit(g.download_track_list)
 
 
 if __name__ == "__main__":
     db.connect()
+    thread_pool = ThreadPoolExecutor(max_workers=MAX_WORKERS)
     # db.create_tables([GenreDB, TrackDB])
     genres = get_files_from_dir(TOPS_PATH, ".json")
     download_all_top_100(genres)
+
     thread_pool.shutdown()
--- threads.py
+++
@@ -1,5 +0,0 @@
-from concurrent.futures import ThreadPoolExecutor
-
-from constants import MAX_WORKERS
-
-thread_pool = ThreadPoolExecutor(max_workers=MAX_WORKERS)
